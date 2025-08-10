e44048383e1bdb1d27785a2683e45e2c318de734
BapamaEmma <bapamaemma10@gmail.com>
2025-08-10 20:11:27 +0000

    Come On

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+// ignore_for_file: avoid_print
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:mindease_app/Splashscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp();
+    print("Firebase initialized successfully!");
     runApp(const MyApp());
   } catch (e) {
+    print("Firebase initialization failed: $e");
     runApp(ErrorApp(error: e.toString()));
   }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,6 +44,7 @@ class ErrorApp extends StatelessWidget {
           child: Text(
             '❌ Firebase failed to initialize:\n$error',
             textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.red),
           ),
         ),
       ),
