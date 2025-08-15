c50c1553224bea14b27d3e0a8b6660d506b6ef14
Jeremiah <jeremiah.opokuboateng@gmail.com>
2025-08-15 06:53:48 +0000

    Traditional Sigup and sign in Complete

--- lib/Signup.dart
+++ lib/Signup.dart
@@ -14,10 +14,10 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
   String Name = '', Age = '', Email = '', Password = '';
-  TextEditingController nameController = new TextEditingController();
-  TextEditingController ageController = new TextEditingController();
-  TextEditingController emailController = new TextEditingController();
-  TextEditingController passwordController = new TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   registration() async {
     if (Password != "" && Email != "" && Password != "") {
